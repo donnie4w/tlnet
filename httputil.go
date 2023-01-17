fad6bcac6b87e528a8f7942b656b91a7414c7f79
donnie <donnie4w@gmail.com>
2023-01-17 17:09:44 +0800

    v1

--- httputil.go
+++ httputil.go
@@ -1,15 +1,8 @@
 package tlnet
 
 import (
-	"errors"
-	"fmt"
-	"io"
-	"regexp"
-
-	"net"
 	"net/http"
-	"os"
-	"strings"
+	"regexp"
 
 	"github.com/donnie4w/simplelog/logging"
 )
@@ -30,46 +23,6 @@ func (this *httpObj) GetCookie(key string) (_r string, err error) {
 func (this *httpObj) SetCookie(k, v string) {
 	cookie := http.Cookie{Name: k, Value: v, Path: "/", MaxAge: 86400}
 	http.SetCookie(this.w, &cookie)
-}
-func getIP(r *http.Request) (string, error) {
-	ip := r.Header.Get("X-Real-IP")
-	if net.ParseIP(ip) != nil {
-		return ip, nil
-	}
-	ip = r.Header.Get("X-Forward-For")
-	for _, i := range strings.Split(ip, ",") {
-		if net.ParseIP(i) != nil {
-			return i, nil
-		}
-	}
-	ip, _, err := net.SplitHostPort(r.RemoteAddr)
-	if err != nil {
-		return "", err
-	}
-	if net.ParseIP(ip) != nil {
-		return ip, nil
-	}
-	return "", errors.New("no valid ip found")
-}
-
-func FormFile(r *http.Request, prefix, fileId, saveFilePath string) (filename string, err error) {
-	defer myRecover()
-	file, fileHeader, er := r.FormFile(fileId)
-	if er != nil {
-		err = er
-		logging.Error(er.Error())
-		return
-	}
-	defer file.Close()
-	// rand.Seed(time.Now().Unix())
-	filename = fmt.Sprint(prefix, "_", fileHeader.Filename)
-	f, er := os.Create(fmt.Sprint(saveFilePath, "/", filename))
-	err = er
-	if er == nil {
-		defer f.Close()
-		_, err = io.Copy(f, file)
-	}
-	return
 }
 
 func myRecover() {
