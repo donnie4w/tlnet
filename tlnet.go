2a86f3304821614682322afdc8015821a3967063
donnie4w <donnie4w@gmail.com>
2024-03-14 23:24:46 +0800

    Update tlnet.go

--- tlnet.go
+++ tlnet.go
@@ -19,100 +19,100 @@ import (
 
 var logger = logging.NewLogger().SetFormat(logging.FORMAT_DATE | logging.FORMAT_TIME | logging.FORMAT_MICROSECNDS).SetLevel(logging.LEVEL_ERROR)
 
-func (this *Tlnet) Handle(pattern string, handlerFunc func(hc *HttpContext)) {
-	this.AddHandlerFunc(pattern, nil, func(w http.ResponseWriter, r *http.Request) {
+func (t *Tlnet) Handle(pattern string, handlerFunc func(hc *HttpContext)) {
+	t.AddHandlerFunc(pattern, nil, func(w http.ResponseWriter, r *http.Request) {
 		handlerFunc(newHttpContext(w, r))
 	})
 }
 
-func (this *Tlnet) POST(pattern string, handlerFunc func(hc *HttpContext)) {
-	this._methodpattern[pattern] = http.MethodPost
-	this.Handle(pattern, handlerFunc)
+func (t *Tlnet) POST(pattern string, handlerFunc func(hc *HttpContext)) {
+	t._methodpattern[pattern] = http.MethodPost
+	t.Handle(pattern, handlerFunc)
 }
 
-func (this *Tlnet) PATCH(pattern string, handlerFunc func(hc *HttpContext)) {
-	this._methodpattern[pattern] = http.MethodPatch
-	this.Handle(pattern, handlerFunc)
+func (t *Tlnet) PATCH(pattern string, handlerFunc func(hc *HttpContext)) {
+	t._methodpattern[pattern] = http.MethodPatch
+	t.Handle(pattern, handlerFunc)
 }
 
-func (this *Tlnet) PUT(pattern string, handlerFunc func(hc *HttpContext)) {
-	this._methodpattern[pattern] = http.MethodPut
-	this.Handle(pattern, handlerFunc)
+func (t *Tlnet) PUT(pattern string, handlerFunc func(hc *HttpContext)) {
+	t._methodpattern[pattern] = http.MethodPut
+	t.Handle(pattern, handlerFunc)
 }
 
-func (this *Tlnet) DELETE(pattern string, handlerFunc func(hc *HttpContext)) {
-	this._methodpattern[pattern] = http.MethodDelete
-	this.Handle(pattern, handlerFunc)
+func (t *Tlnet) DELETE(pattern string, handlerFunc func(hc *HttpContext)) {
+	t._methodpattern[pattern] = http.MethodDelete
+	t.Handle(pattern, handlerFunc)
 }
 
-func (this *Tlnet) GET(pattern string, handlerFunc func(hc *HttpContext)) {
-	this._methodpattern[pattern] = http.MethodGet
-	this.Handle(pattern, handlerFunc)
+func (t *Tlnet) GET(pattern string, handlerFunc func(hc *HttpContext)) {
+	t._methodpattern[pattern] = http.MethodGet
+	t.Handle(pattern, handlerFunc)
 }
 
-func (this *Tlnet) OPTIONS(pattern string, handlerFunc func(hc *HttpContext)) {
-	this._methodpattern[pattern] = http.MethodOptions
-	this.Handle(pattern, handlerFunc)
+func (t *Tlnet) OPTIONS(pattern string, handlerFunc func(hc *HttpContext)) {
+	t._methodpattern[pattern] = http.MethodOptions
+	t.Handle(pattern, handlerFunc)
 }
 
-func (this *Tlnet) HEAD(pattern string, handlerFunc func(hc *HttpContext)) {
-	this._methodpattern[pattern] = http.MethodHead
-	this.Handle(pattern, handlerFunc)
+func (t *Tlnet) HEAD(pattern string, handlerFunc func(hc *HttpContext)) {
+	t._methodpattern[pattern] = http.MethodHead
+	t.Handle(pattern, handlerFunc)
 }
 
-func (this *Tlnet) TRACE(pattern string, handlerFunc func(hc *HttpContext)) {
-	this._methodpattern[pattern] = http.MethodTrace
-	this.Handle(pattern, handlerFunc)
+func (t *Tlnet) TRACE(pattern string, handlerFunc func(hc *HttpContext)) {
+	t._methodpattern[pattern] = http.MethodTrace
+	t.Handle(pattern, handlerFunc)
 }
 
-func (this *Tlnet) CONNECT(pattern string, handlerFunc func(hc *HttpContext)) {
-	this._methodpattern[pattern] = http.MethodConnect
-	this.Handle(pattern, handlerFunc)
+func (t *Tlnet) CONNECT(pattern string, handlerFunc func(hc *HttpContext)) {
+	t._methodpattern[pattern] = http.MethodConnect
+	t.Handle(pattern, handlerFunc)
 }
 
-func (this *Tlnet) HandleWebSocket(pattern string, handlerFunc func(hc *HttpContext)) {
-	this._wss = append(this._wss, newWsStub(pattern, &wsHandler{httpContextFunc: handlerFunc}))
+func (t *Tlnet) HandleWebSocket(pattern string, handlerFunc func(hc *HttpContext)) {
+	t._wss = append(t._wss, newWsStub(pattern, &wsHandler{httpContextFunc: handlerFunc}))
 }
 
-func (this *Tlnet) HandleWebSocketBindOrigin(pattern, origin string, handlerFunc func(hc *HttpContext)) {
-	this._wss = append(this._wss, newWsStub(pattern, &wsHandler{httpContextFunc: handlerFunc, _Origin: origin}))
+func (t *Tlnet) HandleWebSocketBindOrigin(pattern, origin string, handlerFunc func(hc *HttpContext)) {
+	t._wss = append(t._wss, newWsStub(pattern, &wsHandler{httpContextFunc: handlerFunc, _Origin: origin}))
 }
 
-func (this *Tlnet) HandleWebSocketBindOriginFunc(pattern string, handlerFunc func(hc *HttpContext), originFunc func(origin *url.URL) bool) {
-	this._wss = append(this._wss, newWsStub(pattern, &wsHandler{httpContextFunc: handlerFunc, _OriginFunc: originFunc}))
+func (t *Tlnet) HandleWebSocketBindOriginFunc(pattern string, handlerFunc func(hc *HttpContext), originFunc func(origin *url.URL) bool) {
+	t._wss = append(t._wss, newWsStub(pattern, &wsHandler{httpContextFunc: handlerFunc, _OriginFunc: originFunc}))
 }
 
-func (this *Tlnet) HandleWebSocketBindConfig(pattern string, handlerFunc func(hc *HttpContext), config *WebsocketConfig) {
-	this._wss = append(this._wss, newWsStub(pattern, &wsHandler{httpContextFunc: handlerFunc, _OriginFunc: config.OriginFunc, _Origin: config.Origin, _MaxPayloadBytes: config.MaxPayloadBytes, _OnError: config.OnError, _OnOpen: config.OnOpen}))
+func (t *Tlnet) HandleWebSocketBindConfig(pattern string, handlerFunc func(hc *HttpContext), config *WebsocketConfig) {
+	t._wss = append(t._wss, newWsStub(pattern, &wsHandler{httpContextFunc: handlerFunc, _OriginFunc: config.OriginFunc, _Origin: config.Origin, _MaxPayloadBytes: config.MaxPayloadBytes, _OnError: config.OnError, _OnOpen: config.OnOpen}))
 }
 
-func (this *Tlnet) HandleWithFilter(pattern string, _filter *Filter, handlerFunc func(hc *HttpContext)) {
+func (t *Tlnet) HandleWithFilter(pattern string, _filter *Filter, handlerFunc func(hc *HttpContext)) {
 	if handlerFunc != nil {
-		this.AddHandlerFunc(pattern, _filter, func(w http.ResponseWriter, r *http.Request) {
+		t.AddHandlerFunc(pattern, _filter, func(w http.ResponseWriter, r *http.Request) {
 			handlerFunc(newHttpContext(w, r))
 		})
 	} else {
-		this.AddHandlerFunc(pattern, _filter, nil)
+		t.AddHandlerFunc(pattern, _filter, nil)
 	}
 }
 
-func (this *Tlnet) HandleStatic(pattern, dir string, handlerFunc func(hc *HttpContext)) {
+func (t *Tlnet) HandleStatic(pattern, dir string, handlerFunc func(hc *HttpContext)) {
 	if handlerFunc != nil {
-		this.AddStaticHandler(pattern, dir, nil, func(w http.ResponseWriter, r *http.Request) {
+		t.AddStaticHandler(pattern, dir, nil, func(w http.ResponseWriter, r *http.Request) {
 			handlerFunc(newHttpContext(w, r))
 		})
 	} else {
-		this.AddStaticHandler(pattern, dir, nil, nil)
+		t.AddStaticHandler(pattern, dir, nil, nil)
 	}
 }
 
-func (this *Tlnet) HandleStaticWithFilter(pattern, dir string, _filter *Filter, handlerFunc func(hc *HttpContext)) {
+func (t *Tlnet) HandleStaticWithFilter(pattern, dir string, _filter *Filter, handlerFunc func(hc *HttpContext)) {
 	if handlerFunc != nil {
-		this.AddStaticHandler(pattern, dir, _filter, func(w http.ResponseWriter, r *http.Request) {
+		t.AddStaticHandler(pattern, dir, _filter, func(w http.ResponseWriter, r *http.Request) {
 			handlerFunc(newHttpContext(w, r))
 		})
 	} else {
-		this.AddStaticHandler(pattern, dir, _filter, nil)
+		t.AddStaticHandler(pattern, dir, _filter, nil)
 	}
 }
 
@@ -122,20 +122,20 @@ func SetLogOFF() {
 
 // 数据返回客户端
 // return the data to the client
-func (this *HttpContext) ResponseString(data string) (_r int, err error) {
-	return this.ResponseBytes(http.StatusOK, []byte(data))
+func (t *HttpContext) ResponseString(data string) (_r int, err error) {
+	return t.ResponseBytes(http.StatusOK, []byte(data))
 }
 
-func (this *HttpContext) ResponseBytes(status int, bs []byte) (_r int, err error) {
+func (t *HttpContext) ResponseBytes(status int, bs []byte) (_r int, err error) {
 	defer myRecover()
 	if status == 0 {
 		status = http.StatusOK
 	}
-	this.w.WriteHeader(status)
-	if this.w.Header().Get("Content-Length") == "" {
-		this.w.Header().Add("Content-Length", fmt.Sprint(len(bs)))
+	t.w.WriteHeader(status)
+	if t.w.Header().Get("Content-Length") == "" {
+		t.w.Header().Add("Content-Length", fmt.Sprint(len(bs)))
 	}
-	_r, err = this.w.Write(bs)
+	_r, err = t.w.Write(bs)
 	return
 }
 
@@ -143,74 +143,72 @@ func (this *HttpContext) ResponseBytes(status int, bs []byte) (_r int, err error
 // If there are no values associated with the key, Get returns
 // the empty string. To access multiple values, use the map
 // directly.
-func (this *HttpContext) GetParam(key string) (_r string) {
+func (t *HttpContext) GetParam(key string) (_r string) {
 	defer myRecover()
-	_r = this.r.URL.Query().Get(key)
+	_r = t.r.URL.Query().Get(key)
 	return
 }
 
 // TrimSpace GetParam
-func (this *HttpContext) GetParamTrimSpace(key string) (_r string) {
-	return strings.TrimSpace(this.GetParam(key))
+func (t *HttpContext) GetParamTrimSpace(key string) (_r string) {
+	return strings.TrimSpace(t.GetParam(key))
 }
 
 // PostParam returns the first value for the named component of the query.
 // POST and PUT body parameters take precedence over URL query string values.
 // If key is not present, PostParam returns the empty string.
 // To access multiple values of the same key, call PostParams
-func (this *HttpContext) PostParam(key string) (_r string) {
+func (t *HttpContext) PostParam(key string) (_r string) {
 	defer myRecover()
-	_r = this.r.FormValue(key)
+	_r = t.r.FormValue(key)
 	return
 }
 
 // TrimSpace PostParam
-func (this *HttpContext) PostParamTrimSpace(key string) (_r string) {
-	return strings.TrimSpace(this.PostParam(key))
+func (t *HttpContext) PostParamTrimSpace(key string) (_r string) {
+	return strings.TrimSpace(t.PostParam(key))
 }
 
 /*multiple values of the same key*/
-func (this *HttpContext) PostParams(key string) (_r []string) {
+func (t *HttpContext) PostParams(key string) (_r []string) {
 	defer myRecover()
-	this.r.ParseForm()
-	return this.r.Form[key]
+	t.r.ParseForm()
+	return t.r.Form[key]
 }
 
 // 重定向302
-func (this *HttpContext) Redirect(path string) {
+func (t *HttpContext) Redirect(path string) {
 	defer myRecover()
-	http.Redirect(this.w, this.r, path, http.StatusFound)
-	return
+	http.Redirect(t.w, t.r, path, http.StatusFound)
 }
 
-func (this *HttpContext) RedirectWithStatus(path string, status int) {
+func (t *HttpContext) RedirectWithStatus(path string, status int) {
 	defer myRecover()
-	http.Redirect(this.w, this.r, path, status)
-	return
+	http.Redirect(t.w, t.r, path, status)
 }
 
-func (this *HttpContext) FormFile(key string) (multipart.File, *multipart.FileHeader, error) {
+func (t *HttpContext) FormFile(key string) (multipart.File, *multipart.FileHeader, error) {
 	defer myRecover()
-	return this.r.FormFile(key)
+	return t.r.FormFile(key)
 }
 
-func (this *HttpContext) FormFiles(key string) *multipart.Form {
+func (t *HttpContext) FormFiles(key string) *multipart.Form {
 	defer myRecover()
-	return this.r.MultipartForm
+	return t.r.MultipartForm
 }
 
-func (this *HttpContext) Request() *http.Request {
-	return this.r
+func (t *HttpContext) Request() *http.Request {
+	return t.r
 }
 
-func (this *HttpContext) RequestBody() []byte {
+func (t *HttpContext) RequestBody() []byte {
 	var buf bytes.Buffer
-	io.Copy(&buf, this.r.Body)
+	io.Copy(&buf, t.r.Body)
 	return buf.Bytes()
 }
 
-func (this *HttpContext) Writer() http.ResponseWriter {
-	return this.w
+func (t *HttpContext) Writer() http.ResponseWriter {
+	return t.w
 }
 
 func ParseFormFile(file multipart.File, fileHeader *multipart.FileHeader, savePath, namePrefix string) (fileName string, err error) {
