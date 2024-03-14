e0f024603c7b6dce76c1da542066d94b1793b823
donnie4w <donnie4w@gmail.com>
2024-03-14 23:24:39 +0800

    Update httpserver.go

--- httpserver.go
+++ httpserver.go
@@ -8,7 +8,6 @@ import (
 	"fmt"
 	"net"
 	"net/http"
-	. "net/http"
 	"net/url"
 	"os"
 	"strings"
@@ -31,12 +30,12 @@ type stub struct {
 	_pattern   string
 	_dir       string
 	_filter    *Filter
-	_handler   func(ResponseWriter, *Request)
+	_handler   func(http.ResponseWriter, *http.Request)
 	_processor thrift.TProcessor
 	_ttype     TTYPE
 }
 
-func newStub(pattern, dir string, filter *Filter, handler func(ResponseWriter, *Request), processor thrift.TProcessor, ttype TTYPE) *stub {
+func newStub(pattern, dir string, filter *Filter, handler func(http.ResponseWriter, *http.Request), processor thrift.TProcessor, ttype TTYPE) *stub {
 	if pattern[0] != '/' {
 		panic(fmt.Sprint("pattern error,[", pattern, "] must begin with '/'"))
 	}
@@ -80,14 +79,14 @@ type Tlnet struct {
 }
 
 // TLSConfig optionally provides a TLS configuration for use
-// by ServeTLS and ListenAndServeTLS. Note that this value is
+// by ServeTLS and ListenAndServeTLS. Note that t value is
 // cloned by ServeTLS and ListenAndServeTLS, so it's not
 // possible to modify the configuration with methods like
 // tls.Config.SetSessionTicketKeys. To use
 // SetSessionTicketKeys, use Server.Serve with a TLS Listener
 // instead.
-func (this *Tlnet) TLSConfig(_TLSConfig *tls.Config) {
-	this.Server.TLSConfig = _TLSConfig
+func (t *Tlnet) TLSConfig(_TLSConfig *tls.Config) {
+	t.Server.TLSConfig = _TLSConfig
 }
 
 // ReadTimeout is the maximum duration for reading the entire
@@ -98,8 +97,8 @@ func (this *Tlnet) TLSConfig(_TLSConfig *tls.Config) {
 // decisions on each request body's acceptable deadline or
 // upload rate, most users will prefer to use
 // ReadHeaderTimeout. It is valid to use them both.
-func (this *Tlnet) ReadTimeout(_ReadTimeout time.Duration) {
-	this.Server.ReadTimeout = _ReadTimeout
+func (t *Tlnet) ReadTimeout(_ReadTimeout time.Duration) {
+	t.Server.ReadTimeout = _ReadTimeout
 }
 
 // ReadHeaderTimeout is the amount of time allowed to read
@@ -108,8 +107,8 @@ func (this *Tlnet) ReadTimeout(_ReadTimeout time.Duration) {
 // is considered too slow for the body. If ReadHeaderTimeout
 // is zero, the value of ReadTimeout is used. If both are
 // zero, there is no timeout.
-func (this *Tlnet) ReadHeaderTimeout(_ReadHeaderTimeout time.Duration) {
-	this.Server.ReadHeaderTimeout = _ReadHeaderTimeout
+func (t *Tlnet) ReadHeaderTimeout(_ReadHeaderTimeout time.Duration) {
+	t.Server.ReadHeaderTimeout = _ReadHeaderTimeout
 }
 
 // WriteTimeout is the maximum duration before timing out
@@ -117,16 +116,16 @@ func (this *Tlnet) ReadHeaderTimeout(_ReadHeaderTimeout time.Duration) {
 // request's header is read. Like ReadTimeout, it does not
 // let Handlers make decisions on a per-request basis.
 // A zero or negative value means there will be no timeout.
-func (this *Tlnet) WriteTimeout(_WriteTimeout time.Duration) {
-	this.Server.WriteTimeout = _WriteTimeout
+func (t *Tlnet) WriteTimeout(_WriteTimeout time.Duration) {
+	t.Server.WriteTimeout = _WriteTimeout
 }
 
 // IdleTimeout is the maximum amount of time to wait for the
 // next request when keep-alives are enabled. If IdleTimeout
 // is zero, the value of ReadTimeout is used. If both are
 // zero, there is no timeout.
-func (this *Tlnet) IdleTimeout(_IdleTimeout time.Duration) {
-	this.Server.IdleTimeout = _IdleTimeout
+func (t *Tlnet) IdleTimeout(_IdleTimeout time.Duration) {
+	t.Server.IdleTimeout = _IdleTimeout
 }
 
 // MaxHeaderBytes controls the maximum number of bytes the
@@ -134,54 +133,54 @@ func (this *Tlnet) IdleTimeout(_IdleTimeout time.Duration) {
 // values, including the request line. It does not limit the
 // size of the request body.
 // If zero, DefaultMaxHeaderBytes is used.
-func (this *Tlnet) MaxHeaderBytes(_MaxHeaderBytes int) {
-	this.Server.MaxHeaderBytes = _MaxHeaderBytes
+func (t *Tlnet) MaxHeaderBytes(_MaxHeaderBytes int) {
+	t.Server.MaxHeaderBytes = _MaxHeaderBytes
 }
 
 // 数据库文件路径
-// func (this *tlnet) DBPath(dbPath string) {
-// 	this._dbPath = dbPath
+// func (t *tlnet) DBPath(dbPath string) {
+// 	t._dbPath = dbPath
 // }
 
 // 设置请求body限制
-func (this *Tlnet) SetMaxBytesReader(maxBytes int64) {
-	this._maxBytes = maxBytes
+func (t *Tlnet) SetMaxBytesReader(maxBytes int64) {
+	t._maxBytes = maxBytes
 }
 
 // 处理thrift协议请求
-func (this *Tlnet) AddProcessor(pattern string, processor thrift.TProcessor) {
-	this._addProcessor(pattern, processor, JSON)
+func (t *Tlnet) AddProcessor(pattern string, processor thrift.TProcessor) {
+	t._addProcessor(pattern, processor, JSON)
 }
 
-func (this *Tlnet) AddBinaryProcessor(pattern string, processor thrift.TProcessor) {
-	this._addProcessor(pattern, processor, BINARY)
+func (t *Tlnet) AddBinaryProcessor(pattern string, processor thrift.TProcessor) {
+	t._addProcessor(pattern, processor, BINARY)
 }
 
-func (this *Tlnet) AddCompactProcessor(pattern string, processor thrift.TProcessor) {
-	this._addProcessor(pattern, processor, COMPACT)
+func (t *Tlnet) AddCompactProcessor(pattern string, processor thrift.TProcessor) {
+	t._addProcessor(pattern, processor, COMPACT)
 }
 
-func (this *Tlnet) _addProcessor(pattern string, processor thrift.TProcessor, ttype TTYPE) {
-	// this._processors = append(this._processors, &stub{_pattern: pattern, _processor: processor, _ttype: ttype})
-	this._processors = append(this._processors, newStub(pattern, "", nil, nil, processor, ttype))
+func (t *Tlnet) _addProcessor(pattern string, processor thrift.TProcessor, ttype TTYPE) {
+	// t._processors = append(t._processors, &stub{_pattern: pattern, _processor: processor, _ttype: ttype})
+	t._processors = append(t._processors, newStub(pattern, "", nil, nil, processor, ttype))
 }
 
 // 处理动态请求
-func (this *Tlnet) AddHandlerFunc(pattern string, f *Filter, handlerFunc func(ResponseWriter, *Request)) {
-	// this._handlers = append(this._handlers, &stub{_pattern: pattern, _filter: f, _handler: handlerFunc})
-	this._handlers = append(this._handlers, newStub(pattern, "", f, handlerFunc, nil, 0))
+func (t *Tlnet) AddHandlerFunc(pattern string, f *Filter, handlerFunc func(http.ResponseWriter, *http.Request)) {
+	// t._handlers = append(t._handlers, &stub{_pattern: pattern, _filter: f, _handler: handlerFunc})
+	t._handlers = append(t._handlers, newStub(pattern, "", f, handlerFunc, nil, 0))
 }
 
 // 处理静态页面
-func (this *Tlnet) AddStaticHandler(pattern string, dir string, f *Filter, handlerFunc func(ResponseWriter, *Request)) {
-	this._staticHandlers = append(this._staticHandlers, newStub(pattern, dir, f, handlerFunc, nil, 0))
+func (t *Tlnet) AddStaticHandler(pattern string, dir string, f *Filter, handlerFunc func(http.ResponseWriter, *http.Request)) {
+	t._staticHandlers = append(t._staticHandlers, newStub(pattern, dir, f, handlerFunc, nil, 0))
 }
 
 /**http**/
-func (this *Tlnet) HttpStart(addr string) (err error) {
-	this._Handle()
-	this.Server.Addr = addr
-	err = this.Server.ListenAndServe()
+func (t *Tlnet) HttpStart(addr string) (err error) {
+	t._Handle()
+	t.Server.Addr = addr
+	err = t.Server.ListenAndServe()
 	if err != nil {
 		logger.Error("tlnet start error:", err.Error())
 	}
@@ -189,10 +188,10 @@ func (this *Tlnet) HttpStart(addr string) (err error) {
 }
 
 /**http tls**/
-func (this *Tlnet) HttpStartTLS(addr string, certFile, keyFile string) (err error) {
-	this._Handle()
-	this.Server.Addr = addr
-	err = this.Server.ListenAndServeTLS(certFile, keyFile)
+func (t *Tlnet) HttpStartTLS(addr string, certFile, keyFile string) (err error) {
+	t._Handle()
+	t.Server.Addr = addr
+	err = t.Server.ListenAndServeTLS(certFile, keyFile)
 	if err != nil {
 		logger.Error("tlnet startTLS error:", err.Error())
 	}
@@ -200,50 +199,47 @@ func (this *Tlnet) HttpStartTLS(addr string, certFile, keyFile string) (err erro
 }
 
 /**http tls**/
-func (this *Tlnet) HttpStartTlsBytes(addr string, certBys, keyBys []byte) (err error) {
-	this._Handle()
-	this.Server.Addr = addr
+func (t *Tlnet) HttpStartTlsBytes(addr string, certBys, keyBys []byte) (err error) {
+	t._Handle()
+	t.Server.Addr = addr
 	cfg := &tls.Config{}
 	var cert tls.Certificate
 	if cert, err = tls.X509KeyPair(certBys, keyBys); err == nil {
 		cfg.NextProtos = append(cfg.NextProtos, "http/1.1")
 		cfg.Certificates = append(cfg.Certificates, cert)
-		this.TLSConfig(cfg)
+		t.TLSConfig(cfg)
 		var ln net.Listener
 		if ln, err = net.Listen("tcp", addr); err == nil {
 			defer ln.Close()
 			tlsListener := tls.NewListener(ln, cfg)
-			err = this.Server.Serve(tlsListener)
+			err = t.Server.Serve(tlsListener)
 		}
 	}
 	return
 }
 
 /**close service**/
-func (this *Tlnet) Close() (err error) {
-	if this.Server != nil {
-		err = this.Server.Close()
-		if err != nil {
-			logger.Error("tlnet start error:", err.Error())
-		}
+func (t *Tlnet) Close() (err error) {
+	if t.Server != nil {
+		return t.Server.Close()
 	}
 	return
 }
 
-func (this *Tlnet) _Handle() {
-	for _, s := range this._processors {
-		this._serverMux.Handle(s._pattern, http.StripPrefix(s._pattern, &httpHandler{_maxBytes: this._maxBytes, _stub: s, _tlnet: this}))
+func (t *Tlnet) _Handle() {
+	for _, s := range t._processors {
+		t._serverMux.Handle(s._pattern, http.StripPrefix(s._pattern, &httpHandler{_maxBytes: t._maxBytes, _stub: s, _tlnet: t}))
 	}
-	for _, s := range this._handlers {
-		this._serverMux.Handle(s._pattern, http.StripPrefix(s._pattern, &httpHandler{_maxBytes: this._maxBytes, _stub: s, _tlnet: this}))
+	for _, s := range t._handlers {
+		t._serverMux.Handle(s._pattern, http.StripPrefix(s._pattern, &httpHandler{_maxBytes: t._maxBytes, _stub: s, _tlnet: t}))
 	}
-	for _, s := range this._staticHandlers {
-		this._serverMux.Handle(s._pattern, http.StripPrefix(s._pattern, &httpHandler{_maxBytes: this._maxBytes, _stub: s, _staticHandler: http.FileServer(http.Dir(s._dir)), _tlnet: this}))
+	for _, s := range t._staticHandlers {
+		t._serverMux.Handle(s._pattern, http.StripPrefix(s._pattern, &httpHandler{_maxBytes: t._maxBytes, _stub: s, _staticHandler: http.FileServer(http.Dir(s._dir)), _tlnet: t}))
 	}
-	if len(this._wss) > 0 {
-		for _, s := range this._wss {
+	if len(t._wss) > 0 {
+		for _, s := range t._wss {
 			if s._handler != nil {
-				this._serverMux.Handle(s._pattern, s._handler)
+				t._serverMux.Handle(s._pattern, s._handler)
 			}
 		}
 	}
@@ -256,35 +252,35 @@ type httpHandler struct {
 	_tlnet         *Tlnet
 }
 
-func (this *httpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (t *httpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	path, uri, url_uri := r.URL.Path, r.RequestURI, r.URL.RequestURI()
-	if this._maxBytes > 0 {
-		r.Body = http.MaxBytesReader(w, r.Body, this._maxBytes)
+	if t._maxBytes > 0 {
+		r.Body = http.MaxBytesReader(w, r.Body, t._maxBytes)
 	}
-	if len(this._tlnet._methodpattern) > 0 {
+	if len(t._tlnet._methodpattern) > 0 {
 		if url_uri[0] == '/' {
 			url_uri = url_uri[1:]
 		}
 		method, ok := "", false
 		if a, b := len(uri), len(url_uri); a != b {
 			root := r.RequestURI[:(a - b)]
-			method, ok = this._tlnet._methodpattern[root]
+			method, ok = t._tlnet._methodpattern[root]
 		}
-		if ok && strings.ToUpper(method) != strings.ToUpper(r.Method) {
+		if ok && !strings.EqualFold(method, r.Method) {
 			w.WriteHeader(http.StatusMethodNotAllowed)
 			return
 		}
 	}
 
 	// static
-	if this._staticHandler != nil && this._stub._filter != nil && this._stub._filter.notFoundhandler != nil {
-		dir := this._stub._dir
+	if t._staticHandler != nil && t._stub._filter != nil && t._stub._filter.notFoundhandler != nil {
+		dir := t._stub._dir
 		if dir != "" && dir[len(dir)-1:] != "/" {
 			dir = dir + "/"
 		}
 		if _, err := os.Stat(dir + path); os.IsNotExist(err) {
-			if this._stub._filter.notFoundhandler != nil && this._stub._filter.notFoundhandler(w, r) {
+			if t._stub._filter.notFoundhandler != nil && t._stub._filter.notFoundhandler(w, r) {
 				return
 			}
 		}
@@ -295,28 +291,28 @@ func (this *httpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	} else {
 		filterPath = uri
 	}
-	if this._stub._filter != nil {
-		if this._stub._filter.suffixMap.Len() > 0 {
-			if this._stub._filter._processSuffix(filterPath, w, r) {
+	if t._stub._filter != nil {
+		if t._stub._filter.suffixMap.Len() > 0 {
+			if t._stub._filter._processSuffix(filterPath, w, r) {
 				return
 			}
 		}
-		if this._stub._filter.matchMap.Len() > 0 {
-			if this._stub._filter._processGlobal(filterPath, w, r) {
+		if t._stub._filter.matchMap.Len() > 0 {
+			if t._stub._filter._processGlobal(filterPath, w, r) {
 				return
 			}
 		}
 	}
-	if this._stub._processor != nil {
-		processorHandler(w, r, this._stub._processor, this._stub._ttype)
+	if t._stub._processor != nil {
+		processorHandler(w, r, t._stub._processor, t._stub._ttype)
 		return
 	}
-	if this._stub._handler != nil {
-		this._stub._handler(w, r)
+	if t._stub._handler != nil {
+		t._stub._handler(w, r)
 	}
 
-	if this._staticHandler != nil {
-		this._staticHandler.ServeHTTP(w, r)
+	if t._staticHandler != nil {
+		t._staticHandler.ServeHTTP(w, r)
 	}
 }
 
@@ -329,31 +325,31 @@ type wsHandler struct {
 	_OnOpen          func(hc *HttpContext)
 }
 
-func (this *wsHandler) checkOrigin(config *websocket.Config, req *http.Request) (err error) {
+func (t *wsHandler) checkOrigin(config *websocket.Config, req *http.Request) (err error) {
 	config.Origin, err = websocket.Origin(config, req)
 	if err == nil && config.Origin == nil {
 		return fmt.Errorf("null origin")
 	}
-	if (this._Origin != "" && this._Origin != config.Origin.String()) || (this._OriginFunc != nil && !this._OriginFunc(config.Origin)) {
+	if (t._Origin != "" && t._Origin != config.Origin.String()) || (t._OriginFunc != nil && !t._OriginFunc(config.Origin)) {
 		return fmt.Errorf("error origin")
 	}
 	return err
 }
 
 // ServeHTTP implements the http.Handler interface for a WebSocket
-func (this *wsHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	s := websocket.Server{Handler: this.wsConnFunc, Handshake: this.checkOrigin}
+func (t *wsHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	s := websocket.Server{Handler: t.wsConnFunc, Handshake: t.checkOrigin}
 	s.ServeHTTP(w, req)
 }
 
-func (this *wsHandler) wsConnFunc(ws *websocket.Conn) {
+func (t *wsHandler) wsConnFunc(ws *websocket.Conn) {
 	defer myRecover()
 	defer ws.Close()
 	hc := newHttpContext(nil, ws.Request())
-	ws.MaxPayloadBytes, hc.WS._OnError = this._MaxPayloadBytes, this._OnError
+	ws.MaxPayloadBytes, hc.WS._OnError = t._MaxPayloadBytes, t._OnError
 	hc.WS.Conn = ws
-	if this._OnOpen != nil {
-		go this._OnOpen(hc)
+	if t._OnOpen != nil {
+		go t._OnOpen(hc)
 	}
 	defer hc.WS._onErrorChan()
 	for hc.WS.Error == nil {
@@ -363,13 +359,13 @@ func (this *wsHandler) wsConnFunc(ws *websocket.Conn) {
 			break
 		}
 		hc.WS._rbody = byt
-		if this.httpContextFunc != nil {
-			this.httpContextFunc(hc)
+		if t.httpContextFunc != nil {
+			t.httpContextFunc(hc)
 		}
 	}
 }
 
-func processorHandler(w ResponseWriter, r *Request, processor thrift.TProcessor, _ttype TTYPE) {
+func processorHandler(w http.ResponseWriter, r *http.Request, processor thrift.TProcessor, _ttype TTYPE) {
 	var protocolFactory thrift.TProtocolFactory
 	switch _ttype {
 	case JSON:
