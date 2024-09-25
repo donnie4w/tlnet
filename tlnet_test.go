20b3dd50e059ec654cfa454c68525beaf34653d8
donnie4w <donnie4w@gmail.com>
2024-09-25 17:54:53 +0800

    Update tlnet_test.go

--- tlnet_test.go
+++ tlnet_test.go
@@ -1,157 +1,64 @@
-// Copyright (c) , donnie <donnie4w@gmail.com>
+// Copyright (c) 2023, donnie <donnie4w@gmail.com>
 // All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+// github.como/donnie4w/tlnet
+
 package tlnet
 
 import (
 	"fmt"
-	"io"
-	"net"
-	. "net/http"
-	"testing"
-	"time"
-
 	"github.com/donnie4w/simplelog/logging"
+	"net/http"
+	_ "net/http/pprof"
+	"testing"
 )
 
 func Test_tlnet(t *testing.T) {
-	tlnet := NewTlnet()
-	tlnet.ReadTimeout(10 * time.Second)
-	tlnet.SetMaxBytesReader((1 << 20) * 50)
-	tlnet.HandleStaticWithFilter("/cccc/", "test", notFoundFilter(), nil)
-	tlnet.HandleWithFilter("/3/a", interceptFilter("[a-z]"), func(hc *HttpContext) { hc.ResponseString("hello tlnet,  filter test") })
-	tlnet.AddHandlerFunc("/aaa", nil, aaa)
-	tlnet.AddHandlerFunc("/bbb", notFoundFilter(), aaa)
-	tlnet.AddProcessor("/ppp", nil)
-	tlnet.POST("/notify", notify)
-	tlnet.AddStaticHandler("/", "./", nil, nil)
-	tlnet.HandleWebSocket("/ws", websocketFunc)
-	tlnet.HandleWebSocketBindOrigin("/ws2", "http://tlnet/", websocketFunc)
-	tlnet.HandleWebSocketBindConfig("/ws3", websocketFunc, newWebsocketConfig())
-	tlnet.HttpStart(":8082")
+	tl := NewTlnet()
+	SetLogger(true)
+	tl.GET("/a/", func(hc *HttpContext) { hc.ResponseString("path:" + hc.ReqInfo.Path) })
+	tl.GET("/b/:id/:name/order", func(hc *HttpContext) { hc.ResponseString(hc.GetParam("id") + ":" + hc.GetParam("name")) })
+	tl.GET("/test", func(hc *HttpContext) { hc.ResponseString("uri:/test") })
+	tl.HandleWithFilter("/filter/", interceptFilter(), func(hc *HttpContext) { hc.ResponseString("filter test") })
+	//tl.GET("/", func(hc *HttpContext) { hc.ResponseString("root path:" + hc.ReqInfo.Path) })
+	tl.HandleStaticWithFilter("/img/", "./html/img", interceptFilter(), nil)
+	tl.HttpStart(":8080")
 }
 
-func _Test_tlnet2(t *testing.T) {
-	tlnet := NewTlnet()
-	// tlnet.DBPath("test.db")
-	tlnet.SetMaxBytesReader((1 << 20) * 50)
-	tlnet.Handle("/qq", handleFunc)
-	tlnet.HandleStatic("/s", "test.db", staticHandleFunc)
-	tlnet.HttpStart(":8080")
-}
-
-func handleFunc(hc *HttpContext) {
-	logging.Debug(hc.ReqInfo)
-	logging.Debug(hc.GetParam("name"))
-	// logging.Debug(hc.ReqInfo.Header.Get("X-Real-IP"))
-	// logging.Debug(hc.ReqInfo.Header.Get("X-Forward-For"))
-	logging.Debug(net.SplitHostPort(hc.ReqInfo.RemoteAddr))
-	hc.ResponseString("hello tlnet")
-}
-
-func staticHandleFunc(hc *HttpContext) {
-	logging.Debug(hc.ReqInfo)
-	logging.Debug(hc.GetParam("name"))
-	logging.Debug(net.SplitHostPort(hc.ReqInfo.RemoteAddr))
-	// hc.Redirect("https://baidu.com")
-}
-
-func notFoundFilter() *Filter {
+func interceptFilter() *Filter {
 	f := NewFilter()
-	f.AddPageNotFoundIntercept(notFound)
-	return f
-}
-
-func aaa(w ResponseWriter, r *Request) {
-	logging.Debug("aaa")
-	logging.Debug(fmt.Sprint(r.Header))
-	io.WriteString(w, "hello aaa 你访问成功了")
-}
-
-func notFound(hc *HttpContext) bool {
-	logging.Debug("notFound")
-	logging.Debug(hc.ReqInfo.Header)
-	hc.ResponseString("not found")
-	return true
-}
-
-// 自定义正则拦截
-func interceptFilter(pattern string) (f *Filter) {
-	f = NewFilter()
-	f.AddIntercept(pattern, func(hc *HttpContext) bool {
-		hc.ResponseString(hc.ReqInfo.Uri + " is matched[" + pattern + "]")
+	//内置后缀拦截器
+	f.AddSuffixIntercept([]string{".jpg"}, func(hc *HttpContext) bool {
+		logging.Debug("suffix  path:" + hc.ReqInfo.Path)
+		return false
+	})
+	//内置路径未找到拦截器
+	f.AddPageNotFoundIntercept(func(hc *HttpContext) bool {
+		hc.Error("page not found:"+hc.ReqInfo.Uri, http.StatusNotFound)
 		return true
 	})
-	return
-}
-
-// 后缀为.html的过滤器
-func suffixFilter() *Filter {
-	f := NewFilter()
-	f.AddSuffixIntercept([]string{"html"}, suffixIntercept)
-	return f
-}
-
-func httpFilter() *Filter {
-	f := NewFilter()
-	f.AddIntercept(".html$", func(hc *HttpContext) bool {
-		hc.Redirect("https://github.com")
-		return true
+	//自定义URL正则匹配规则拦截器
+	f.AddIntercept(".*?", func(hc *HttpContext) bool {
+		logging.Debug("intercept:", hc.ReqInfo.Uri)
+		if hc.ReqInfo.Path == "" {
+			hc.Error("path is empty:"+hc.ReqInfo.Uri, http.StatusForbidden)
+			return true
+		}
+		return false
 	})
 	return f
 }
 
-func suffixIntercept(hc *HttpContext) bool {
-	hc.ResponseString("html is not allowed")
-	return true
+func TestWebsocket(t *testing.T) {
+	tl := NewTlnet()
+	tl.HandleWebSocket("/ws", websocketHandler)
+	tl.HttpStart(":8080")
+
 }
-
-func staticFilter() *Filter {
-	f := NewFilter()
-	f.AddPageNotFoundIntercept(notFound)
-	f.AddIntercept("[ab]", globalIntercept)
-	return f
-}
-
-func globalIntercept(hc *HttpContext) bool {
-	hc.ResponseString("globalIntercept")
-	return true
-}
-
-func notify(hc *HttpContext) {
-	for _, v := range wsmap {
-		v.WS.Send(fmt.Sprint("通知：", time.Now()))
-	}
-}
-
-var wsmap = make(map[int64]*HttpContext, 0)
-
-func newWebsocketConfig() *WebsocketConfig {
-	wc := new(WebsocketConfig)
-	wc.MaxPayloadBytes = 1 << 20 * 100
-	wc.OnError = func(ws *Websocket) {
-		logging.Error("err:", ws.Error)
-		err := ws.Close()
-		logging.Error(err)
-	}
-	return wc
-}
-
-func websocketFunc(hc *HttpContext) {
-	_, ok := wsmap[hc.WS.Id]
-	if !ok {
-		wsmap[hc.WS.Id] = hc
-	}
-	logging.Debug(hc.ReqInfo)
-	logging.Debug(hc.ReqInfo.RemoteAddr)
-	// msg := string(hc.WS.Read())
-	logging.Debug(len(hc.WS.Read()))
-	// logging.Debug("收到:", msg)
-	// hc.WS.Send([]byte("好了"))
-	// hc.WS.Send("你发送的是：" + string(hc.WS.Read()))
-	// for k, v := range wsmap {
-	// 	if k != hc.WS.Id {
-	// 		v.WS.Send(fmt.Sprint("这是", hc.WS.Id, "发送的信息:", msg))
-	// 	}
-	// }
-
+func websocketHandler(hc *HttpContext) {
+	wsId := hc.WS.Id                                            //每个websocket链接都会生成一个识别id
+	msg := string(hc.WS.Read())                                 //读取客户端请求数据
+	hc.WS.Send("服务ID" + fmt.Sprint(wsId) + "已经收到你发送的信息:" + msg) //向客户端回复数据
 }
