95c96bd1e7971421b01c8c31b76ce5c9c9e8e6ce
donnie4w <donnie4w@gmail.com>
2024-09-25 17:52:19 +0800

    Create const.go

---
+++ const.go
@@ -0,0 +1,53 @@
+// Copyright (c) 2023, donnie <donnie4w@gmail.com>
+// All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+// github.como/donnie4w/tlnet
+
+package tlnet
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type tf_pco_type byte
+type muxmod byte
+type httpMethod string
+
+const (
+	_ tf_pco_type = iota
+	JSON
+	BINARY
+	COMPACT
+)
+
+const (
+	_ muxmod = iota
+	tlnetmod
+	nativemod
+)
+
+const (
+	_SS = 1 << 18
+	_MS = 1 << 20
+	_LS = 1 << 24
+)
+
+const (
+	HttpGet       httpMethod = http.MethodGet
+	HttpHead      httpMethod = http.MethodHead
+	HttpPost      httpMethod = http.MethodPost
+	HttpPut       httpMethod = http.MethodPut
+	HttpPatch     httpMethod = http.MethodPatch
+	HttpDelete    httpMethod = http.MethodDelete
+	HttpConnect   httpMethod = http.MethodConnect
+	HttpOptions   httpMethod = http.MethodOptions
+	HttpTrace     httpMethod = http.MethodTrace
+	defaultMethod httpMethod = ""
+)
+
+func patternpanic(pattern string) {
+	panic(fmt.Sprint("pattern error,[", pattern, "] must begin with '/'"))
+}
