62371b7272602d6a1ec8f78f752b37888cd6056f
donnie4w <donnie4w@gmail.com>
2025-01-15 10:19:08 +0800

    Create log.go

---
+++ log.go
@@ -0,0 +1,43 @@
+// Copyright (c) 2023, donnie <donnie4w@gmail.com>
+// All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+// github.como/donnie4w/tlnet
+
+package tlnet
+
+import (
+	"log"
+)
+
+func SetLogger(on bool) {
+	logger.SetLogger(on)
+}
+
+var logger = newLog()
+
+type logu struct {
+	IsVaild bool
+	logger  *log.Logger
+}
+
+func newLog() *logu {
+	return &logu{logger: log.New(log.Writer(), "[tlnet]", log.Ldate|log.Ltime)}
+}
+
+func (l *logu) SetLogger(on bool) {
+	l.IsVaild = on
+}
+
+func (l *logu) Debug(v ...interface{}) {
+	if l.IsVaild {
+		l.logger.Println(v...)
+	}
+}
+
+func (l *logu) Error(v ...interface{}) {
+	if l.IsVaild {
+		l.logger.Fatal(v...)
+	}
+}
