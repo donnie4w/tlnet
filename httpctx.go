0b64cca07b8b07b20c509db76ef9d01494170130
donnie4w <donnie4w@gmail.com>
2025-01-15 10:19:01 +0800

    Update httpctx.go

--- httpctx.go
+++ httpctx.go
@@ -8,10 +8,14 @@
 package tlnet
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/donnie4w/gofer/util"
+	"io"
+	"mime/multipart"
 	"net/http"
 	"regexp"
+	"strings"
 )
 
 // HttpInfo is a struct that represents the header information of an HTTP request.
@@ -67,6 +71,128 @@ func (t *HttpContext) GetCookie(name string) (_r string, err error) {
 	}
 	err = er
 	return
+}
+
+// ResponseString
+// return the data to the client
+func (t *HttpContext) ResponseString(data string) (_r int, err error) {
+	return t.ResponseBytes(http.StatusOK, []byte(data))
+}
+
+func (t *HttpContext) ResponseBytes(status int, bs []byte) (_r int, err error) {
+	if status == 0 {
+		status = http.StatusOK
+	}
+	t.w.WriteHeader(status)
+	if len(bs) > 0 {
+		_r, err = t.w.Write(bs)
+	}
+	return
+}
+
+// Error replies to the request with the specified error message and HTTP code.
+// It does not otherwise end the request; the caller should ensure no further
+// writes are done to w.
+// The error message should be plain text.
+func (t *HttpContext) Error(error string, code int) {
+	http.Error(t.Writer(), error, code)
+}
+
+// GetParam gets the first value associated with the given key.
+// If there are no values associated with the key, Get returns
+// the empty string. To access multiple values, use the map
+// directly.
+func (t *HttpContext) GetParam(key string) (_r string) {
+	_r = t.r.URL.Query().Get(key)
+	if logger.IsVaild {
+		logger.Debug("[GetParam] "+key, ", result:", _r)
+	}
+	return
+}
+
+// GetParamTrimSpace TrimSpace GetParam
+func (t *HttpContext) GetParamTrimSpace(key string) (_r string) {
+	_r = strings.TrimSpace(t.GetParam(key))
+	if logger.IsVaild {
+		logger.Debug("[GetParamTrimSpace] "+key, ", result:", _r)
+	}
+	return
+}
+
+// PostParam returns the first value for the named component of the query.
+// POST and PUT body parameters take precedence over URL query string values.
+// If key is not present, PostParam returns the empty string.
+// To access multiple values of the same key, call PostParams
+func (t *HttpContext) PostParam(key string) (_r string) {
+	_r = t.r.FormValue(key)
+	if logger.IsVaild {
+		logger.Debug("[PostParam] "+key, ", result:", _r)
+	}
+	return
+}
+
+// PostParamTrimSpace TrimSpace PostParam
+func (t *HttpContext) PostParamTrimSpace(key string) (_r string) {
+	_r = strings.TrimSpace(t.PostParam(key))
+	if logger.IsVaild {
+		logger.Debug("[PostParamTrimSpace] "+key, ", result:", _r)
+	}
+	return
+}
+
+// PostParams
+// multiple values of the same key
+func (t *HttpContext) PostParams(key string) (_r []string) {
+	t.r.ParseForm()
+	_r = t.r.Form[key]
+	if logger.IsVaild {
+		logger.Debug("[PostParams] "+key, ", result:", _r)
+	}
+	return
+}
+
+// Redirect
+// 重定向 302
+func (t *HttpContext) Redirect(path string) {
+	if logger.IsVaild {
+		logger.Debug("[Redirect] " + path)
+	}
+	http.Redirect(t.w, t.r, path, http.StatusFound)
+}
+
+func (t *HttpContext) RedirectWithStatus(path string, status int) {
+	if logger.IsVaild {
+		logger.Debug("[RedirectWithStatus] "+path, ",status:", status)
+	}
+	http.Redirect(t.w, t.r, path, status)
+}
+
+func (t *HttpContext) FormFile(key string) (multipart.File, *multipart.FileHeader, error) {
+	if logger.IsVaild {
+		logger.Debug("[FormFile] " + key)
+	}
+	return t.r.FormFile(key)
+}
+
+func (t *HttpContext) FormFiles(key string) *multipart.Form {
+	if logger.IsVaild {
+		logger.Debug("[FormFiles] " + key)
+	}
+	return t.r.MultipartForm
+}
+
+func (t *HttpContext) Request() *http.Request {
+	return t.r
+}
+
+func (t *HttpContext) RequestBody() []byte {
+	var buf bytes.Buffer
+	io.Copy(&buf, t.r.Body)
+	return buf.Bytes()
+}
+
+func (t *HttpContext) Writer() http.ResponseWriter {
+	return t.w
 }
 
 func (t *HttpContext) SetCookie(name, value, path string, maxAge int) {
